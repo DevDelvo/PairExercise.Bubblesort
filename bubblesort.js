a185e5205ab7a247486e09ca4e9a9479295b2b9c
Wen Zhang <zhangwenfd@gmail.com>
2018-04-23 13:11:46 -0400

    almost there

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,4 @@
- // function bubbleSort(array) {
+// function bubbleSort(array) {
 //     let counter = 0;
 //     for (let i = 0; i < array.length; i++) {
 //       for (let j = 1; j < array.length; j++) {
@@ -19,30 +19,27 @@
 //   array[index2] = temp;
 // }
 
-class BubbleSort {
-  constructor(array) {
-    this.array = array;
-    this.counter = 0;
-  }
+// class BubbleSort {
+//   constructor(array) {
+//     this.array = array;
+//     this.counter = 0;
+//   }
 
-  bubbleSorter() {
-    var array = this.array;
-      let counter = 0;
-      for (let i = 0; i < array.length; i++) {
-        for (let j = 1; j < array.length; j++) {
-          if (array[j-1] > array[j]) {
-            this.swap(array, j-1, j);
-            this.counter++;
-          }
-        }
+function bubbleSort(array) {
+  // let counter = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        this.swap(array, j - 1, j);
+        // this.counter++;
       }
-      console.log(this.counter);
-      return this.array;
+    }
   }
+  return array;
+}
 
-  swap(array, index1, index2) {
-    let temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
-  }
+function swap(array, index1, index2) {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,24 +1,56 @@
-describe('Bubble Sort', function(){
-
-  beforeAll(function() {
-    spyOn(BubbleSort, 'BubbleSort.swap').and.callThrough();
-  });
-  it('counts the swaps', function() {
-    BubbleSort.bubbleSorter();
-    expect( new BubbleSort([4,5,2]).bubbleSorter()).calls.count().toEqual(2);
+describe("Bubble Sort", function() {
+  let array, array2, array3;
+  beforeEach(() => {
+    array = [];
+    array2 = [5, 4, 2];
+    array3 = [4, 5, 2, 8, 7, 6, 1, 3];
   });
 
-
-  it('handles an empty array', function(){
-    expect( new BubbleSort([]).bubbleSorter()).toEqual( [] );
+  it("has methods `bubbleSorter` ,and `swap`", () => {
+    expect(typeof bubbleSort).toBe("function");
+    expect(typeof swap).toBe("function");
   });
 
-  it('handles an array with three elements', function(){
-    expect( new BubbleSort([4,5,2]).bubbleSorter() ).toEqual( [2,4,5] );
+  it("handles an empty array", function() {
+    expect(bubbleSort(array)).toEqual([]);
   });
 
-  it('handles an array with eight elements', function(){
-    expect( new BubbleSort([4,5,2,8,7,6,1,3]).bubbleSorter() ).toEqual( [1,2,3,4,5,6,7,8] );
+  it("handles an array with three elements", function() {
+    expect(bubbleSort(array2)).toEqual([2, 4, 5]);
+    // expect(array2.swap()).toEqual(2);
   });
 
+  it("handles an array with eight elements", function() {
+    expect(bubbleSort(array3)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  // beforeAll(function() {
+
+  // });
+  it("counts the swaps", function() {
+    spyOn(window, "swap").and.callThrough();
+    bubbleSort(array2);
+    expect(window.swap.calls.count().toEqual(2));
+  });
+
+  // it("handles an empty array", function() {
+  //   expect(new BubbleSort([]).bubbleSorter()).toEqual([]);
+  // });
+
+  // it("handles an array with three elements", function() {
+  //   expect(new BubbleSort([4, 5, 2]).bubbleSorter()).toEqual([2, 4, 5]);
+  // });
+
+  // it("handles an array with eight elements", function() {
+  //   expect(new BubbleSort([4, 5, 2, 8, 7, 6, 1, 3]).bubbleSorter()).toEqual([
+  //     1,
+  //     2,
+  //     3,
+  //     4,
+  //     5,
+  //     6,
+  //     7,
+  //     8
+  //   ]);
+  // });
 });
